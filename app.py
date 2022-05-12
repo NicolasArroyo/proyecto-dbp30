46e7cd0ad769a97603b8717c65ab0b0f5003a7d2
matiasmjcm <52078856+matiasmjcm@users.noreply.github.com>
2022-05-12 18:46:14 -0500

    app con login

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 import sys
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:1234@localhost:5432/project_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Account.query.get(int(user_id))
+
 
 class Account(db.Model):
+    __tablename__= "accounts"
     id = db.Column(db.Integer, nullable=False, primary_key=True)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
@@ -48,16 +62,40 @@ class Author(db.Model):
     books = db.relationship("Book", backref="author")
 
 
-
-
 @app.route('/home')
 def home():
     return render_template('home.html')
 
 
-@app.route('/login')
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Account.query.filter_by(username=form.username.data).first()
+        if user:
+            password = Account.query.filter_by(password=form.password.data).first()
+            if password:
+                login_user(user)
+                return redirect(url_for('home'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 
 
 if __name__ == "__main__":
