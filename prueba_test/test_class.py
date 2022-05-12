cda42963b830a77bee9d11000831149be709aef5
matiasmjcm <52078856+matiasmjcm@users.noreply.github.com>
2022-05-12 18:20:48 -0500

    pruba_unit_test

---
+++ prueba_test/test_class.py
@@ -0,0 +1,6 @@
+#monkie patch 
+from main import calculadora
+def test_class_monkey(monkeypatch): 
+    monkeypatch.setattr(calculadora,"sumar", lambda x:4)
+    c = calculadora()
+    assert c.sumar() == 4
