e2893158fb7f477ba6a4714fea1eacb4c97d4663
matiasmjcm <52078856+matiasmjcm@users.noreply.github.com>
2022-05-13 11:13:10 -0500

    remove prueba_test 1

--- prueba_test/test_class.py
+++
@@ -1,6 +0,0 @@
-#monkie patch 
-from main import calculadora
-def test_class_monkey(monkeypatch): 
-    monkeypatch.setattr(calculadora,"sumar", lambda x:4)
-    c = calculadora()
-    assert c.sumar() == 4
