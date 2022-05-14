f8702a8ede9fe5adf51021e9bc0209da1b0947ac
NicolasArroyo <nicolas.arroyo.c@utec.edu.pe>
2022-05-14 14:46:45 -0500

    <CREATE> Se ha creado la plantilla HTML del register y se adicionan los endpoints.

--- app.py
+++ app.py
@@ -48,8 +48,6 @@ class Author(db.Model):
     books = db.relationship("Book", backref="author")
 
 
-
-
 @app.route('/home')
 def home():
     return render_template('home.html')
@@ -58,6 +56,40 @@ def home():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/register/newUser', methods=['POST'])
+def registerNewUser():
+    try:
+
+        requestData = request.get_json()
+        firstName = requestData["firstName"]
+        lastName = requestData["lastName"]
+        username = requestData["username"]
+        password = requestData["password"]
+        email = requestData["email"]
+
+        q = db.session.query(Account.id).filter(Account.username == username)
+        if (db.session.query(q.exists()).scalar()):
+            pass
+        else:
+            account = Account(first_name=firstName, last_name=lastName, username=username, password=password,
+                              number_of_sanctions=0, is_active=True, email=email)
+            db.session.add(account)
+            db.session.commit()
+
+    except Exception as e:
+        print(e)
+        print(sys.exc_info())
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return "success"
 
 
 if __name__ == "__main__":
